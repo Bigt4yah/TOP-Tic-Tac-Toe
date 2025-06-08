a1226e2c933426cfdde972891de4ce47b312b60a
Troy Donovan <bigt4yah@Troys-MacBook-Pro.local>
2025-06-08 14:05:52 -0400

    Logic creation WIP. Need to move win condition checks to the gameController object

---
+++ ttt.js
@@ -0,0 +1,173 @@
+/* Goals:
+    Build an interactive Tic-Tac-Toe game
+
+    Store the gameboard as an array inside of a Gameboard object  -- DONE
+
+Tips:
+  1. Use as little global code as possible
+  2. Try putting as many items inside factory functions as possible
+  3. If a single instance of something is needed (like the gameboard, the displayController, etc)
+      then wrap the factory inside an IIFE (module pattern) so it cannot be reused
+       to create additional instances
+  4. Focus on getting the game working in console first
+    4a. Check for all winning 3-in-a-rows and ties
+  5. Once the game works in console, create an object that will handle the display/DOM logic
+  6. Write a function that will render the contents of the gameboard array to the webpage
+  7. Write the functions that will allow players to add marks to a specific spot on the board
+    by interacting with the appropriate DOM elements 
+    7a. Don't forget the logic that keeps players from playing in spots that are already taken
+  8. Clean up the interace that allows players to put in their names, include a button to start/restart the game
+    8a. Add a display element that shows the results upon game end
+*/
+
+/* MISC notes:
+
+-- We can have a function provide a numerical value and type of player piece (X or O) to the gameboard. Based on this numerical value, set the space on the gameboard to the player piece
+
+*/
+
+// Create the Gameboard object
+const gameBoard = (function () {
+    let board = ["","","","","","","","",""];
+
+    let winCondition1 = [1,2,3];
+    let winCondition2 = [4,5,6];
+    let winCondition3 = [7,8,9];
+
+    let winCondition4 = [1,4,7];
+    let winCondition5 = [2,5,8];
+    let winCondition6 = [3,6,9];
+    
+    let winCondition7 = [1,5,9];
+    let winCondition8 = [3,5,7];
+
+    let allWinConditions = [winCondition1, winCondition2, winCondition3, winCondition4, winCondition4, winCondition5, winCondition6, winCondition7, winCondition8];
+
+    let winHappened;
+    let gameOver = false;
+
+    // function to get the marker in the current slot
+    function getSlot(slot) {
+        return board[slot];
+    }
+
+    // function to set the marker in the current slot
+    function setMarker(slot, marker){
+        if (getSlot(slot) == "") {
+            board[slot] = marker;
+            if(checkWin()){
+                gameOver = true;
+                return "${marker}'s win!";
+            }
+            else if(checkDraw()){
+                gameOver = true;
+                return "Draw!";
+            };
+        }
+        else {
+            return "Slot already taken";
+        }
+    }
+
+    // function to reset the board
+    function resetBoard(){
+        board = ["","","","","","","","",""];
+    }
+
+    // function to get the board
+    function getBoard(){
+        return board;
+    }
+
+    // function to check if a win condition has occurred
+    function checkWinCondition(currentWinCondition){
+        let currentValue = board[currentWinCondition[0]];
+
+        if(currentValue != ""){
+            for(let i = 0; i < currentWinCondition.length; i++){
+                if(board[currentWinCondition[i]] == ""){
+                    break;
+                    return false;
+                }
+                else if(board[currentWinCondition[i]] == currentValue){
+                    continue;
+                }
+            }
+        }
+        else{
+            return false;
+        }
+        return true;
+    }
+
+    // function to evaluate all win conditions
+    function checkWin(){
+        winHappened =  allWinConditions.some(checkWinCondition);
+        return winHappened;
+    }
+
+    // function to check for a draw
+    function checkDraw(){
+        for(let i =1; i<board.length; i++){
+            if(board[i] == ""){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // function to return gameStatus
+    function getGameStatus(){
+        return gameOver;
+    }
+
+    return {getBoard, resetBoard, getSlot, setMarker, getGameStatus};
+    
+})();
+
+// Create a gameController object
+const gameController = (function() {
+
+    // initially player 1's turn
+    let currentTurn;
+
+    // function to check if a player win happened
+    function evaluatePlayerWin(){
+        return gameBoard.getGameStatus();
+    }
+
+    // function to start the game
+    function startGame(){
+        currentTurn = 1;
+    }
+
+    // function to change the currentTurn
+    function changeTurn(){
+        if(!evaluatePlayerWin()){
+            if(currentTurn == 1){
+            currentTurn = 2;
+            }
+            else{
+                currentTurn = 1;
+                return "Player {currentTurn}'s turn";
+            }
+        }
+        else{
+            return "The game ended";
+        }
+    }   
+
+    // function to restart the game
+    function restartGame(){
+        gameBoard.resetBoard();
+    }
+
+    return {startGame, changeTurn};
+
+})();
+
+
+// function to create the player object
+function Player(markerValue){
+
+}
